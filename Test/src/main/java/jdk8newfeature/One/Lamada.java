b1ab2908fa96740d70aefe1a26c1c0994d439586
fanwenhao <fanwenhao121@163.com>
2017-04-28 17:30:45 +0800

    JDK8新特性探索

---
+++ Test/src/main/java/jdk8newfeature/One/Lamada.java
@@ -0,0 +1,13 @@
+package jdk8newfeature.One;
+
+/**
+ * TODO
+ *
+ * @author fanwh
+ * @version V1.0
+ * @create 2017-04-27 19:19
+ *  
+ */
+public class Lamada {
+
+}
---
+++ Test/src/main/java/jdk8newfeature/One/package-info.java
@@ -0,0 +1,17 @@
+/**
+ * TODO
+ *
+ * @author fanwh
+ * @version V1.0
+ * @create 2017-04-27 18:14
+ *  
+ */
+/**
+ * Lambda表达式与Functional接口
+ * Lambda表达式（也称为闭包）是整个Java 8发行版中最受期待的在Java语言层面上的改变，
+ * Lambda允许把函数作为一个方法的参数（函数作为参数传递进方法中），
+ * 或者把代码看成数据：函数式程序员对这一概念非常熟悉。
+ * 在JVM平台上的很多语言（Groovy，Scala，……）从一开始就有Lambda，
+ * 但是Java程序员不得不使用毫无新意的匿名类来代替lambda
+ */
+package jdk8newfeature.One;
